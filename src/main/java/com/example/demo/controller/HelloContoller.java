93c55a90ed97a680c2181363bb93ccd1630c0cc7
Sreenivas Kanthamneni <sreenivaskanthamneni@gmail.com>
2019-10-06 00:19:37 -0500

    initial commit

---
+++ src/main/java/com/example/demo/controller/HelloContoller.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloContoller {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @PostMapping("/hello")
+    public String sayHello(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
